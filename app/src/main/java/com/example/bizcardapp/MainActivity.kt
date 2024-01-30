ac74a42051156adce087f47da7d66757819d92bb
stefan bayne <stefanbayne@gmail.com>
2024-01-30 17:35:57 -0500

    added logic for state.

--- app/src/main/java/com/example/bizcardapp/MainActivity.kt
+++ app/src/main/java/com/example/bizcardapp/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.example.bizcardapp
 
-import android.graphics.drawable.Drawable
-import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -9,35 +7,33 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ButtonElevation
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.graphics.Color.Companion.LightGray
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.layout.ContentScale
@@ -62,6 +58,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
+    val buttonClicked = remember { mutableStateOf(false) }
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -85,25 +82,55 @@ fun CreateBizCard() {
                 CreateImageProfile()
                 Divider(thickness = 2.dp)
                 CreateInfo()
-                CreateButton()
+                ElevatedButton(
+                    onClick = {
+                        buttonClicked.value = !buttonClicked.value
+                    },
+                    modifier = Modifier
+                        .padding(3.dp),
+                    elevation = ButtonDefaults.buttonElevation(defaultElevation = 10.dp)
+                ) {
+                    Text(text = "Create", style = MaterialTheme.typography.labelLarge)
+                }
+                if (buttonClicked.value) {
+                    Content()
+                    Log.d("BUTTON", "Button Clicked")
+                } else {
+                    Box {}
+                }
             }
         }
     }
 }
 
+@Preview
 @Composable
-private fun CreateButton() {
-    Column(
-        modifier = Modifier.size(200.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+fun Content() {
+    Box(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .padding(5.dp)
     ) {
-        ElevatedButton(
-            onClick = { Log.d("Button-Click", "Button clicked!") },
+        Surface(
             modifier = Modifier
-                .padding(3.dp),
-            elevation = ButtonDefaults.buttonElevation(defaultElevation = 10.dp)
+                .padding(3.dp)
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 3.dp, color = Color.DarkGray)
         ) {
-            Text(text = "Create", style = MaterialTheme.typography.labelLarge)
+            Portfolio(data = listOf("Project 1", "Project 2", "Project 3", "Project 4"))
+
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn {
+        items(data) { item ->
+            Text(item)
         }
     }
 }
